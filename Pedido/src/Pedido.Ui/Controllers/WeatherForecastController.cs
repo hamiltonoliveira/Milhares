3ad8fd084cfc256475a51db4dd6f706625ce3178
Hamilton Vale <hamilton.valnet@gmail.com>
2023-07-07 18:39:09 -0300

    criação de sub-pastas. dockerfile, docker-compose

---
+++ Pedido/src/Pedido.Ui/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Pedido.Ui.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            var stop = Stopwatch.StartNew();
+            try { 
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            }
+            finally
+            {
+                stop.Stop();
+                Console.WriteLine($"Took:{stop.ElapsedMilliseconds}");
+            }
+        }   
+    }
+}
