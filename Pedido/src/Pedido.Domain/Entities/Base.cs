0d1b92074c218c6ae81c41b078f8167c99aa49a8
Hamilton Vale <hamilton.valnet@gmail.com>
2023-07-10 13:23:33 -0300

    gitignore

--- Pedido/src/Pedido.Domain/Entities/Base.cs
+++ Pedido/src/Pedido.Domain/Entities/Base.cs
@@ -3,6 +3,7 @@
     public abstract class Base
     {
         public int Id { get; set; }
+        public Guid IdGuid { get; set; } = Guid.NewGuid();
         public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
         public DateTime DataAlteracao { get; set; } = DateTime.UtcNow;
         public Boolean Ativo { get; set; }
